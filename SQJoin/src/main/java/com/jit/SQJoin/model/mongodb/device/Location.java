4d49d88ae8c66ef73ace82b0d82b087aa4d4507b
dxlkc <43257580+dxlkc@users.noreply.github.com>
2019-10-09 11:12:42 +0800

    alert some mistakes

--- SQJoin/src/main/java/com/jit/SQJoin/model/mongodb/device/Location.java
+++ SQJoin/src/main/java/com/jit/SQJoin/model/mongodb/device/Location.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Location {
     //省 市 区
-    private String privince;
+    private String province;
     private String city;
     private String area;
 }
